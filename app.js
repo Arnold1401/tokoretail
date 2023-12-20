8adcf340bac4300093ca43d8eb361ae5dddd5a1a
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2023-12-20 10:44:09 +0700

    commitawal

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Define a simple route
+app.get("/", (req, res) => {
+  res.send("Hello, this is a simple Express.js app!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
